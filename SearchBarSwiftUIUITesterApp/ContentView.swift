ff60aec70a61b5ee5b628390de42d05b2b26e8ee
Kavinda Dilshan <kdtechniques@gmail.com>
2024-08-03 00:04:07 +0530

    Initial Commit

---
+++ SearchBarSwiftUIUITesterApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SearchBarSwiftUIUITesterApp
+//
+//  Created by Mr. Kavinda Dilshan on 2024-08-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SearchBarSwiftUIUITesterApp/SearchBarSwiftUIUITesterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarSwiftUIUITesterAppApp.swift
+//  SearchBarSwiftUIUITesterApp
+//
+//  Created by Mr. Kavinda Dilshan on 2024-08-03.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarSwiftUIUITesterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
